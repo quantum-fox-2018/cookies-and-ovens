1acabc522178f3c618e19ad54f754f15611e3b9a
Taufik Hidayat <ovick_kd@yahoo.com>
2018-03-13 23:32:08 +0700

    cookies and ovens

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -8,3 +8,84 @@
 //
 //
 // Your code here
+
+class bake {
+
+     constructor(name, time){
+
+         this.name = name
+         this.time = 0
+         this.done = time
+         this.status = this.status()
+
+     }
+
+     addTime(){
+
+         this.time += 5
+
+     }
+
+     status (){
+
+       for (;this.time <= this.done;){
+
+         if (this.time == this.done-5 ){
+
+             console.log(this.name+', menit ke-'+this.time+' : hampir matang')
+
+         } else if (this.time < this.done){
+
+             console.log(this.name+', menit ke-'+this.time+' : mentah')
+
+         } else if (this.time == this.done){
+
+             console.log(this.name+', menit ke-'+this.time+' : matang')
+
+         } else if(this.time > this.done){
+
+             console.log(this.name+', menit ke-'+this.time+' : hangus')
+
+         }
+
+         this.addTime()
+
+       }
+
+     }
+
+ }
+
+ class Kacang extends bake {
+
+     constructor(){
+
+        super('Kue Kacang',20)
+
+     }
+
+ }
+
+ class Coklat extends bake {
+
+     constructor(){
+
+        super('Kue Coklat',30)
+
+     }
+
+ }
+
+ class Keju extends bake {
+
+     constructor(){
+
+        super('Kue Keju',35)
+
+     }
+
+ }
+
+ let kacang = new Kacang()
+ let coklat = new Coklat()
+ let keju = new Keju()
