f8ad7d592ae23e1cc49dc61b73e2bffeb948498b
barestu <fheartnet@gmail.com>
2018-03-13 22:07:26 +0700

    Tugas Cookies And Ovens

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -1,6 +1,7 @@
 // Answer These Questions:
 //
 // - What are essential classes?
+//    Cookie, ChocolateChip, PeanutButter, CheeseChip
 // - What attributes will each class have?
 // - What interface will each class provide?
 // - How will the classes interact with each other?
@@ -8,3 +9,60 @@
 //
 //
 // Your code here
+
+class Cookie {
+  constructor(name, cookedTime) {
+    this.name = name;
+    this.status = 'mentah';
+    this.cooked_time = cookedTime;
+  }
+}
+
+class ChocolateChip extends Cookie {
+  constructor() {
+    super('Chocolate Chip', 20);
+  }
+}
+
+class PeanutButter extends Cookie {
+  constructor() {
+    super('Peanut Butter', 30)
+  }
+}
+
+class CheeseCake extends Cookie {
+  constructor() {
+    super('Cheese Cake', 35)
+  }
+}
+
+class Oven {
+  bakes(cookie, bakesTime) {
+    console.log(`Start Baking ${cookie.name}!`);
+    for (let i = 0; i <= bakesTime; i+=5) {
+      if (i === cookie.cooked_time) {
+        cookie.status = 'matang';
+        console.log(`${cookie.name}, menit ke ${i} : ${cookie.status}`);
+      } else if (i > cookie.cooked_time) {
+        cookie.status = 'hangus'
+        console.log(`${cookie.name}, menit ke ${i} : ${cookie.status}`);
+      } else if (i < 15) {
+        cookie.status = 'mentah'
+        console.log(`${cookie.name}, menit ke ${i} : ${cookie.status}`);
+      } else {
+        cookie.status = 'hampir matang'
+        console.log(`${cookie.name}, menit ke ${i} : ${cookie.status}`);
+      }
+    }
+    console.log(`Done!\n`);
+  }
+}
+
+let chocoChip = new ChocolateChip();
+let peanutButter = new PeanutButter();
+let cheeseCake = new CheeseCake();
+let oven = new Oven();
+
+oven.bakes(chocoChip, 20);
+oven.bakes(peanutButter, 25);
+oven.bakes(cheeseCake, 40);
