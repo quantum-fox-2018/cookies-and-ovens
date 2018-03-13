40e2b51bf3950a2f0a224f1d8b494b6e232963a7
NeilTanado <neiltanado@yahoo.co.id>
2018-03-13 23:52:09 +0700

    first push

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -1,3 +1,4 @@
+/*jshint esversion:6*/
 // Answer These Questions:
 //
 // - What are essential classes?
@@ -8,3 +9,51 @@
 //
 //
 // Your code here
+
+"use strict";
+
+class Bake{
+  constructor (cakeName , timeCook){
+    this.name = cakeName;
+    this.timeCook = timeCook;
+    this.status = 'mentah';
+  }
+
+  static bakeProcess(){
+    for(let i = 0 ; i < this.timeCook ; i+=5){
+      if(i <= this.timeCook -5){
+        this.status = 'Hampir Matang';
+      }
+      else if(i === this.timeCook){
+        this.status = 'Matang';
+      }
+      else if(i>this.timeCook){
+        this.status = 'Hangus';
+      }
+    }
+  }
+}
+
+
+  class ChocolateCake extends Bake{
+  constructor(cakeName,timeCook){
+    super(cakeName,20);
+  }
+}
+
+class PeanutCake extends Bake{
+  constructor(cakeName,timeCook){
+    super(cakeName,25);
+  }
+}
+
+class CheeseCake extends Bake{
+  constructor(cakeName,timeCook){
+    super(cakeName,30);
+  }
+}
+
+let coklat = new ChocolateCake()
+for(let i = 0 ; i <= coklat.timeCook ;i+=5){
+  console.log('Kue Coklat Waktu ke - ' + i + ' Statusnya ' + bakeProcess(i))
+}
