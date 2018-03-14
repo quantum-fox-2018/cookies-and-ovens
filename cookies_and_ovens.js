de7ae2f10684c25b43ad8c140148e610656078c4
ihsanmaulanahamdani <ihsanmaulana5@gmail.com>
2018-03-14 15:03:50 +0700

    release-0

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -8,3 +8,62 @@
 //
 //
 // Your code here
+
+class Cookies {
+    constructor(name, time, status) {
+        this.cake_name = name;
+        this.cake_time = time;
+        this.cake_status = status;
+    }
+}
+
+class PeanutCake extends Cookies {
+    constructor() {
+        super('Peanut Cake', 30, 'mentah');
+    }
+}
+
+class ChocolateCake extends Cookies {
+    constructor() {
+        super('Chocolate Cake', 20, 'mentah');
+    }
+}
+
+class CheeseCake extends Cookies {
+    constructor() {
+        super('Cheese Cake', 35, 'mentah');
+    }
+}
+
+class Oven {
+    constructor(name, time) {
+        this.cake = name;
+        this.baking_time = time;
+        this.arrayOfCake = [];
+    }
+
+    bake() {
+
+        for (let i = 0; i < this.baking_time; i += 5) {
+            if (this.cake.cake_name === 'Peanut Cake') {
+                if (i + 5 < 15) {
+                    this.arrayOfCake.push(`Kue kacang, menit ke ${i+5} : ${this.cake.cake_status}`);
+                }
+            } else if (this.cake.cake_name === 'Chocolate Cake') {
+                this.arrayOfCake.push('Kue coklat,');
+            } else if (this.cake.cake_name === 'Cheese Cake') {
+                this.arrayOfCake.push('Kue keju,');
+            }
+        }
+
+        return this.arrayOfCake.join('\n');
+    }
+}
+
+let peanut_cake = new PeanutCake();
+let chocolate_cake = new ChocolateCake();
+let cheese_cake = new CheeseCake();
+
+let oven = new Oven(peanut_cake, 20);
+
+console.log(oven.bake());
