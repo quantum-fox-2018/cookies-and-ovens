db246cec69b7d442efc5f75bfb232b34b3fc5efa
Rafie Gilang <gilang0293@gmail.com>
2018-03-14 11:59:26 +0700

    membenarkan konsep aggregation di code

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -1,56 +1,72 @@
 class Kue{
-  constructor(cooking_time, name, done){
-    this.cooking_time = cooking_time;
-    this.status = null;
+  constructor(name, done){
+    this.status = 'mentah';
     this.name = name;
     this.done = done;
   }
 
   changeStatus(currentTime){
-    if(currentTime < this.done-10){
-      this.status = 'mentah';
-    } else if(currentTime <= this.done-5){
-      this.status = 'hampir matang';
-    }else if(currentTime >= this.done+10){
-      this.status = 'hangus';
-    }else if(currentTime >= this.done){
-      this.status = 'matang';
+    switch(true){
+      case currentTime < this.done-10 :
+        this.status = 'mentah';
+        break;
+      case currentTime <= this.done-5 :
+        this.status = 'hampir matang';
+        break;
+      case currentTime >= this.done+10 :
+        this.status = 'hangus';
+        break;
+      case currentTime >= this.done :
+        this.status = 'matang';
+        break;
     }
   }
 
+}
+
+class Oven{
+  constructor(kue_kue, waktuMasak){
+    this.kue_kue = kue_kue;
+    this.waktuMasak = waktuMasak;
+  }
+
+
   masakKue(){
-    for(let time = 5; time <= this.cooking_time; time+=5){
-      this.changeStatus(time);
-      console.log(`${this.name}, menit ke ${time} : ${this.status}`)
+    for(let time = 5; time <= this.waktuMasak; time+=5){
+      for(let indexKue = 0; indexKue < this.kue_kue.length; indexKue++){
+        kue_kue[indexKue].changeStatus(time);
+        console.log(`${kue_kue[indexKue].name}, menit ke ${time} : ${kue_kue[indexKue].status}`)
+      }
+      console.log('');
     }
   }
 }
 
 class KueCoklat extends Kue{
-  constructor(cooking_time){
-    super(cooking_time, 'Kue coklat', 25);
+  constructor(){
+    super('Kue coklat', 25);
 
   }
 }
 
 class KueKacang extends Kue{
-  constructor(cooking_time){
-    super(cooking_time, 'Kue kacang', 30);
+  constructor(){
+    super('Kue kacang', 30);
   }
 }
 
 class KueKeju extends Kue{
-  constructor(cooking_time){
-    super(cooking_time, 'Kue keju', 35);
+  constructor(){
+    super('Kue keju', 35);
   }
 }
 
-let kue_coklat = new KueCoklat(35);
-let kue_kacang = new KueKacang(45);
-let kue_keju = new KueKeju(50);
+let kue_coklat = new KueCoklat();
+let kue_kacang = new KueKacang();
+let kue_keju = new KueKeju();
 
-kue_coklat.masakKue();
-console.log('');
-kue_kacang.masakKue();
-console.log('');
-kue_keju.masakKue();
+let kue_kue = [kue_coklat, kue_kacang, kue_keju];
+
+let oven = new Oven(kue_kue, 60);
+
+oven.masakKue();
