fb4beadff80e1e6812f80bccb0ccb708ebd9dc15
Natascha Lamsu <tazcha15@gmail.com>
2018-03-16 06:30:11 +0700

    cookies and ovens

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -8,3 +8,60 @@
 //
 //
 // Your code here
+class Cookie {
+  constructor(cookie, time, status) {
+    this.cookie = cookie;
+    this.bake_time = time;
+    this.bake_status = 'mentah';
+  }
+}
+
+class Choco extends Cookie {
+  constructor(cookie, time, status) {
+    super(cookie, status);
+    this.bake_time  = 20;
+  }
+}
+
+class Peanut extends Cookie {
+  constructor(cookie, time, status) {
+    super(cookie, status)
+    this.bake_time = 30;
+  }
+}
+
+class Cheese extends Cookie {
+  consructor(cookie, time, status) {
+    super(cookie, status)
+    this.bake_time = 35
+  }
+}
+
+class Oven {
+  constructor(bakeCookie) {
+    this.tray = [];
+  }
+
+  addCookie(cookie) {
+    this.tray.push(cookie);
+  }
+
+  bakingCookies() {
+    for(var timer = 0; timer < this.tray[j].bake_time; timer+=5) {
+      console.log('timer : ', timer)
+      for(var i = 0; i < this.tray.length; i++) {
+        if(this.tray[i].baketime ===  timer+5) {
+          this.tray[i].bake_status = 'hampir matang';
+        } else if(this.tray[i].baketime ===  timer) {
+          this.tray[i].bake_status = 'matang';
+        } else if(this.tray[i].baketime <  timer) {
+          this.tray[i].bake_status = 'hangus';
+        }
+        console.log(this.tray[i].cookie, this.tray[i].status);
+      }
+    }
+  }
+}
+
+var choco = new Cookie('Choco Cookie');
+console.log(choco);
